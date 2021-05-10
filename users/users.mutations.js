412e07a18f4684690a32f6c69e6b992daee5bb7d
idoburnish <69456626+idoburnish@users.noreply.github.com>
2021-05-10 22:26:07 +0900

    #2.3 seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,28 +1,43 @@
-import client from "../client.js"
+import bcrypt from "bcrypt";
+import client from "../client.js";
 
 export default {
     Mutation: {
         createAccount: async (
-            _, 
+            _,  
             {firstName, lastName, userName, email, password} // data from user
-        ) => {
-            // 1) check if userName of email are already on DB.
-            const existingUser = await client.user.findFirst({
-                where: {
-                    OR: [
-                        {
-                            userName: userName,
-                        },
-                        {
-                            email,
-                        },
-                    ]
+        ) => { 
+            try {
+                // check if userName of email are already on DB.
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            { 
+                                userName, 
+                            },
+                            { 
+                                email, 
+                            },
+                        ],
+                    },
+                });
+                if (existingUser) {
+                    throw new Error("This username/email is already taken.");
                 }
-            })
-            console.log(existingUser);
-            // 2) if not, hash password
-            
-            // 3) then save and return the user
+                // if not, hash password
+                const uglyPassword = await bcrypt.hash(password, 10);
+                // then save and return the user
+                return client.user.create({
+                    data: {
+                        userName, 
+                        email, 
+                        firstName, 
+                        lastName,
+                        password: uglyPassword
+                }});
+            } catch (e) {
+                return e; 
+            }
         },
     },
 };
