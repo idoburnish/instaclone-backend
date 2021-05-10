02c4d359a7926d9f55509b019288864cf13e2047
idoburnish <69456626+idoburnish@users.noreply.github.com>
2021-05-11 01:54:03 +0900

    #2.6 divide and conquer

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import client from "../client.js";
+import client from "../../client.js";
 
 export default {
     Mutation: {
@@ -39,33 +38,6 @@ export default {
             } catch (e) {
                 return e; 
             }
-        },
-
-        login: async (_, {userName, password}) => {
-            // find user with args.userName
-            const user = await client.user.findFirst({where:{userName}})
-            if (!user) {
-                return{
-                    ok: false,
-                    error: "User not found"
-                };
-            }
-
-            // if userName exist, check password with args.password
-            const passwordOK = await bcrypt.compare(password, user.password)
-            if (!passwordOK) {
-                return {
-                    ok: false,
-                    error: "Incorrect password"
-                };
-            }
-
-            // if correct password, issue a token amd send it to the user
-            const token = await jwt.sign({id:user.id}, process.env.SECRET_KEY);   
-            return {
-                ok: true,
-                token,
-            };
         },
     },
 };
