0e8f028196712e9aa0173971705335e44d8cd71f
idoburnish <69456626+idoburnish@users.noreply.github.com>
2021-05-11 02:32:05 +0900

    #2.7 updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,36 @@
+import bcrypt from "bcrypt";
+import { buildClientSchema } from "graphql";
+import client from "../../client.js";
+
 export default {
     Mutation: {
-        editProfile: () => console.log("hi"),
+        editProfile: async (_, { firstName, lastName, userName, email, password:newPassword }) => {
+            let uglyPassword = null;
+            if (newPassword) {
+                uglyPassword = await bcrypt.hash(newPassword, 10);
+            }
+            const updatedUser =  await client.user.update({
+                where: {
+                    id: 1
+                }, 
+                data:{
+                    firstName, 
+                    lastName, 
+                    userName, 
+                    email, 
+                    ...(uglyPassword && { password: uglyPassword }),
+                }
+            });
+            if (updatedUser.id) {
+                return {
+                    ok: true
+                };
+            } else {
+                return {
+                    ok: false,
+                    error: "Could not update profile."
+                };
+            }
+        }
     },
 };
