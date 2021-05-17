0ed87fd6ceab43f2682f1996dba51d4b1daa6d86
idoburnish <69456626+idoburnish@users.noreply.github.com>
2021-05-18 02:34:58 +0900

    #2.18 Ejecting from Apollo Server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import {typeDefs, resolvers} from "./schema.js";
 import { getUser, protectResolver } from "./users/users.utils.js";
 
@@ -16,6 +17,9 @@ const server = new ApolloServer({
   }
 });
 
-server
-  .listen(PORT)
-  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({app});
+app.listen({port:PORT}, () => {
+    console.log(`Server is running on http://localhost:${PORT}/graphql`);
+})
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,3 +1,4 @@
+import {createWriteStream} from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client.js";
 import { protectedResolver } from "../users.utils.js";
@@ -8,8 +9,9 @@ const resolverFn = async (
     {loggedInUser}
 ) => {
     const {filename, createReadStream} = await avatar;
-    const stream = createReadStream();
-    console.log(stream);
+    const readStream = createReadStream();
+    const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+    readStream.pipe(writeStream);
     let uglyPassword = null;
     if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
